e4514f6e809a1e3e6d98034beb06b51303afed1d
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-02-07 15:36:50 -0500

    rendering an expanded summary

--- index.js
+++ index.js
@@ -31,6 +31,11 @@ const resultsWrapper = document.querySelector('.results')
 const onInput = async event => {
   const movies = await fetchData(event.target.value)
 
+  if (!movies.length) {
+    dropdown.classList.remove('is-active')
+    return
+  }
+
   resultsWrapper.innerHTML = ''
   dropdown.classList.add('is-active')
   for (const movie of movies) {
@@ -41,8 +46,48 @@ const onInput = async event => {
     <img src = "${imgSrc}" />
     <p>${movie.Title}</p>
     `
+    option.addEventListener('click', () => {
+      dropdown.classList.remove('is-active')
+      input.value = movie.Title
+      onMovieSelect(movie)
+    })
 
     resultsWrapper.appendChild(option)
   }
 }
 input.addEventListener('input', debounce(onInput, 500))
+
+document.addEventListener('click', event => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active')
+  }
+})
+
+const onMovieSelect = async movie => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: '2bc3fe43',
+      i: movie.imdbID
+    }
+  })
+  document.querySelector('#summary').innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = movieDetail => {
+  return `
+  <article class="media">
+    <figure class="media-left">
+    <p class="image">
+      <img src="${movieDetail.Poster}" />
+    </p>
+    </figure>
+    <div class="media-content">
+      <div class="content">
+      <h1>${movieDetail.Title}</h1>
+      <h4>${movieDetail.Genre}</h4>
+      <p>${movieDetail.Plot}</p>
+      </div>
+    </div>
+  </article>
+  `
+}

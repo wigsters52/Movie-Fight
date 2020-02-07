a8477a3f434757d816b254259ebd19d447d3bdc4
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-02-07 12:49:28 -0500

    Added error handling, menu drop down, moved HTML to javascript to be created by javascript

--- index.js
+++ index.js
@@ -5,10 +5,27 @@ const fetchData = async searchTerm => {
       s: searchTerm
     }
   })
+
+  if (response.data.Error) {
+    return []
+  }
   return response.data.Search
 }
+const root = document.querySelector('.autocomplete')
+
+root.innerHTML = `
+  <label><b>Search for a Movie</b></label>
+  <input class="input"/>
+  <div class="dropdown">
+    <div class="dropdown-menu">
+      <div class="dropdown-content results"></div>
+    </div>
+  </div>
+`
 
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
 
 const onInput = async event => {
   const movies = await fetchData(event.target.value)

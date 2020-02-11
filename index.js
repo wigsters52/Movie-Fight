74354f3302ae045949cd5c478d98b0995d4a9ba5
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-02-10 20:54:10 -0500

    added multiple search forms

--- index.js
+++ index.js
@@ -11,56 +11,14 @@ const fetchData = async searchTerm => {
   }
   return response.data.Search
 }
-const root = document.querySelector('.autocomplete')
-
-root.innerHTML = `
-  <label><b>Search for a Movie</b></label>
-  
-  <div class="dropdown">
-  <input class="input"/>
-    <div class="dropdown-menu">
-      <div class="dropdown-content results"></div>
-    </div>
-  </div>
-`
-
-const input = document.querySelector('input')
-const dropdown = document.querySelector('.dropdown')
-const resultsWrapper = document.querySelector('.results')
-// const dropdownResults = document.querySelector('.dropdown-content')
-const onInput = async event => {
-  const movies = await fetchData(event.target.value)
-
-  if (!movies.length) {
-    dropdown.classList.remove('is-active')
-    return
-  }
-
-  resultsWrapper.innerHTML = ''
-  dropdown.classList.add('is-active')
-  for (const movie of movies) {
-    const option = document.createElement('a')
-    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
-    option.classList.add('dropdown-item')
-    option.innerHTML = `
-    <img src = "${imgSrc}" />
-    <p>${movie.Title}</p>
-    `
-    option.addEventListener('click', () => {
-      dropdown.classList.remove('is-active')
-      input.value = movie.Title
-      onMovieSelect(movie)
-    })
-
-    resultsWrapper.appendChild(option)
-  }
-}
-input.addEventListener('input', debounce(onInput, 500))
-
-document.addEventListener('click', event => {
-  if (!root.contains(event.target)) {
-    dropdown.classList.remove('is-active')
-  }
+createAutoComplete({
+  root: document.querySelector('.autocomplete')
+})
+createAutoComplete({
+  root: document.querySelector('.autocomplete-two')
+})
+createAutoComplete({
+  root: document.querySelector('.autocomplete-three')
 })
 
 const onMovieSelect = async movie => {
@@ -88,6 +46,26 @@ const movieTemplate = movieDetail => {
       <p>${movieDetail.Plot}</p>
       </div>
     </div>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Awards}</p>
+    <p class="subtitle">Awards</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.BoxOffice}</p>
+    <p class="subtitle">Box Office</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.Metascore}</p>
+    <p class="subtitle">Metascore</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbRating}</p>
+    <p class="subtitle">IMDB Rating</p>
+  </article>
+  <article class="notification is-primary">
+    <p class="title">${movieDetail.imdbVotes}</p>
+    <p class="subtitle">IMDB Votes</p>
   </article>
   `
 }

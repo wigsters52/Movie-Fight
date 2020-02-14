dea32287497a384f9f48b19ce718de34f421ef31
Ryan Wiggins <ryanwiggins52@gmail.com>
2020-02-13 20:38:00 -0500

    comparison functionality added

--- index.js
+++ index.js
@@ -63,7 +63,25 @@ const onMovieSelect = async (movie, summaryElement, side) => {
 }
 
 const runComparsion = () => {
-  console.log('Time for comparsion')
+  const leftSideStats = document.querySelectorAll('#left-summary .notification')
+  const rightSideStats = document.querySelectorAll(
+    '#right-summary .notification'
+  )
+
+  leftSideStats.forEach((leftStat, index) => {
+    const rightStat = rightSideStats[index]
+
+    const leftSideValue = parseInt(leftStat.dataset.value)
+    const rightSideValue = parseInt(rightStat.dataset.value)
+
+    if (rightSideValue > leftSideValue) {
+      leftStat.classList.remove('is-primary')
+      leftStat.classList.add('is-warning')
+    } else {
+      rightStat.classList.remove('is-primary')
+      rightStat.classList.add('is-warning')
+    }
+  })
 }
 
 const movieTemplate = movieDetail => {
